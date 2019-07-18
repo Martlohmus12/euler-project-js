ff29d1152c0e1aa3bf9c59d98b8f3d85a45ea135
Märt Lõhmus <martlohmus@Marts-MacBook-Pro.local>
2019-07-19 00:58:25 +0300

    create new folder for new euler projec

--- scripts/new-folder.sh
+++ scripts/new-folder.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
-echo "What's your name?"
-echo $process
+
+mkdir $PWD/src/euler-$1
+cd $PWD/src/euler-$1
+touch index.js && echo "const main = () => {}\n\nexport { main }; " > index.js
+
+touch index.spec.js && 
+echo "import { main } from \"./index.js\";\n
+test(\"test\", () => {
+  var t0 = performance.now();
+  const anwser = main();
+  var t1 = performance.now();\n
+  console.log(\"Call to main took \" + (t1 - t0) + \" milliseconds.\");\n
+  expect(anwser).toBe();
+});" > index.spec.js
+
+touch index.txt
