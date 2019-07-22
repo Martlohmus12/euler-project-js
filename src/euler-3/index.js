223accb004f368777270e2bece7c252f1654f136
mrt <martlohmus12@gmail.com>
2019-07-22 23:44:17 +0200

    solution for problem 3

--- src/euler-3/index.js
+++ src/euler-3/index.js
@@ -1,6 +1,43 @@
 const main = number => {
-  
-  return number;
+
+	let lastNumber = number;
+
+	while (lastNumber > 2 ) {
+		lastNumber = getLargerPrimeNumber(lastNumber);
+		const prime = isPrime(lastNumber);
+
+		if (prime) {
+			break;
+		}
+ 	}
+
+	return lastNumber;
 };
 
-export { main };
+function getLargerPrimeNumber(number) {
+  let i = 2;
+
+  while (number > 1) {
+
+  	if (number % i == 0) {
+      number = number / i;
+      i = i - 1;
+  	}
+
+    i = i + 1;
+  }
+
+  return i;
+}
+
+function isPrime(num) {
+  for (let i = 2, s = Math.sqrt(num); i <= s; i++) {
+    if (num % i === 0) {
+      return false;
+   	}
+  }
+
+  return num > 1;
+}
+	
+export { main }	
--- src/euler-3/index.spec.js
+++ src/euler-3/index.spec.js
@@ -1,5 +1,15 @@
 import { main } from "./index.js";
 
+test("should return the largest prime factor of 15", function() {
+  var t0 = performance.now();
+  const anwser = main(15);
+  var t1 = performance.now();
+
+  console.log("Call to main took " + (t1 - t0) + " milliseconds.");
+
+  expect(anwser).toEqual(5);
+});
+
 test("should return the largest prime factor of 13195", function() {
   var t0 = performance.now();
   const anwser = main(13195);
